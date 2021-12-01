100baf6028a77b2c3b3c7edbf8b68dbf0d21e3ca
anonymous anonymous <rohan.bhatt@kellogg.northwestern.edu>
2021-12-01 22:39:49 +0000

    Whitelist columns in Mood for ActiveAdmin

--- app/admin/moods.rb
+++ app/admin/moods.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Mood do
 
+ permit_params :mood
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
