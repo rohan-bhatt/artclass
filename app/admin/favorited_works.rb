671f589cafdf1350bf9a7f6d24afc2f1b2b0ef00
anonymous anonymous <rohan.bhatt@kellogg.northwestern.edu>
2021-12-01 22:39:56 +0000

    Whitelist columns in FavoritedWork for ActiveAdmin

--- app/admin/favorited_works.rb
+++ app/admin/favorited_works.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FavoritedWork do
 
+ permit_params :artwork_id, :creator_id, :favoriter_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
