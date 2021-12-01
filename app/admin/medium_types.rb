5eb1eb5dbc6f320470c75f467b83637c58c1caad
anonymous anonymous <rohan.bhatt@kellogg.northwestern.edu>
2021-12-01 22:39:54 +0000

    Whitelist columns in MediumType for ActiveAdmin

--- app/admin/medium_types.rb
+++ app/admin/medium_types.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MediumType do
 
+ permit_params :medium
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
