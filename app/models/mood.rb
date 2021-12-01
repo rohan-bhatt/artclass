503dd928eccaf6aa248df43e0e876bea59a3cdac
anonymous anonymous <rohan.bhatt@kellogg.northwestern.edu>
2021-12-01 22:38:55 +0000

    Generate moods resource with ideas:scaffold

---
+++ app/models/mood.rb
@@ -0,0 +1,14 @@
+class Mood < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    mood
+  end
+
+end
---
+++ app/views/moods/_mood.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! mood, :id, :mood, :created_at, :updated_at
+json.url mood_url(mood, format: :json)
