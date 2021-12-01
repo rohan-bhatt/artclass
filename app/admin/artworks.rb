ead58c52450800afe3b47810e70adad671849e9f
anonymous anonymous <rohan.bhatt@kellogg.northwestern.edu>
2021-12-01 22:39:52 +0000

    Whitelist columns in Artwork for ActiveAdmin

--- app/admin/artworks.rb
+++ app/admin/artworks.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Artwork do
 
+ permit_params :image, :artist_id, :title, :description, :dimensions_height, :dimensions_width, :medium_id, :mood_id, :prompt_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
